7d7a48270dd17db9fc7d06117a2f5b2fccda9434
guialvesds <guilherme_ealves@outlook.com>
2023-04-07 19:36:08 -0300

    feat(auth): Implements authenticate login JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  login(user: User): UserToken {
+    const payload: UserPayload = {
+      sub: user.id,
+      name: user.primary_name,
+      email: user.email + ' ' + user.second_name,
+    };
+
+    const jwt = this.jwtService.sign(payload);
+
+    return {
+      access_token: jwt,
+    };
+  }
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
