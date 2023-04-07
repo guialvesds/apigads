e3cf676fc7888bab89864646a89b254d5b512b9a
guialvesds <guilherme_ealves@outlook.com>
2023-04-07 18:59:08 -0300

    feat(auth): Implemets local strategie

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  validateUser(email: string, password: string) {
-    throw new Error('Method not implemented.');
+  constructor(private userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      //Verificamos se a hash fornecida pelo front está de acordo com a do usuário.
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+    // Caso não seja encontrado o e-mail informado ou a senha esteja errada.
+    throw new Error('E-mail ou senha inválido.');
   }
 }
