c48de79b27c04537552def2b9dfde4ea16e89d04
guialvesds <guilherme_ealves@outlook.com>
2023-04-07 18:33:01 -0300

    feat(auth): implement login, guard and localStrategie

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ControllerModule } from './controller/controller.module';
-import { ServiceModule } from './service/service.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
-  imports: [ControllerModule, ServiceModule]
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}
