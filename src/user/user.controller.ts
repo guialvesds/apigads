407c0a938c59c8ffd0e7e4abb9f587262664bd93
guialvesds <guilherme_ealves@outlook.com>
2023-04-07 14:59:16 -0300

    feat('user'): Implement "FindByEmail'

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -25,6 +25,11 @@ export class UserController {
     return this.userService.findAll();
   }
 
+  @Get(':email')
+  findByEmail(@Param('email') email: string) {
+    return this.userService.findByEmail(email);
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
