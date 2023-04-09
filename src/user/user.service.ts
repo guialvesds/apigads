a058242390b023ba7172a3b1a27da08f02ef0e7f
guialvesds <guilherme_ealves@outlook.com>
2023-04-08 23:27:30 -0300

    feat(prisma schema): Implements 'Modelagem de banco de dados'

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from 'src/prisma-service/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { User } from './entities/user.entity';
 
